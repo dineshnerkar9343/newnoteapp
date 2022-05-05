f2f135173594ac053f5b12e3c629c0f18af67ebf
dinesh <dineshnerkar92@gmail.com>
2022-05-05 10:13:34 +0530

    notestate notecontext addd

--- src/App.js
+++ src/App.js
@@ -1,27 +1,30 @@
 
 import './App.css';
 
-import {
+import{
   BrowserRouter,
   Routes,
   Route,
 } from "react-router-dom";
+
 import Navbar from './components/Navbar';
 import Home from './components/Home';
 import About from './components/About';
+import NoteState from './context/NoteState';
 
 function App() {
   return (
     <>
+    <NoteState>
   <BrowserRouter>
+  <Navbar/>
   <Routes>
-    <Navbar/>
-    <Route index element={<Home/>} />
-    <Route index element={<About/>} />
+    <Route exact path="/home" element={<Home/>}></Route>
+    <Route exact path="/about" element={<About/>}></Route>
 
   </Routes>
-
   </BrowserRouter>
+  </NoteState>
 
     </>
   );
--- src/components/About.js
+++ src/components/About.js
@@ -1,9 +1,18 @@
-import React from 'react'
+import React, { useContext, useEffect } from 'react'
+import NoteContext from '../context/NoteContext'
+
+
 
 const About = () => {
+  const a = useContext(NoteContext)
+
+  useEffect(() => {
+    a.update()
+  }, [])
+
   return (
 <>
-<p>This is about page</p>
+<p>This is about page, hi i am {a.state.name}, my job role is {a.state.role}</p>
 </>
   )
 }
